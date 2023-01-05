356b968c4a9bc5a452ee628f16aed7ddfc014fa5
kaseeswarareddy <118353221+kaseeswarareddy@users.noreply.github.com>
2023-01-05 19:05:25 +0530

    commit

--- main.tf
+++ main.tf
@@ -4,10 +4,15 @@ access_key = "AKIA5YMYAGZAMPHRM7UV"
 secret_key = "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"
 }
 
-resource "aws_instance" "two" {
+resource "aws_instance" "one" {
 ami = "ami-00d8a762cb0c50254"
 instance_type = "t2.medium"
 tags = {
-Name = "rama"
+Name = "prema"
 }
 }
+
+resource "aws_s3_bucket" "two" {
+bucket = "rajani"
+}
+
