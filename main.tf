2c5d642669d1027252d7811ba9619a82ed1af292
kaseeswarareddy <118353221+kaseeswarareddy@users.noreply.github.com>
2023-01-05 18:48:04 +0530

    change

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ access_key = "AKIA5YMYAGZAMPHRM7UV"
 secret_key = "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"
 }
 
-resource "aws_instance" "one" {
+resource "aws_instance" "two" {
 ami = "ami-00d8a762cb0c50254"
 instance_type = "t2.medium"
 tags = {
-Name = "jyothi"
+Name = "rama"
 }
 }
